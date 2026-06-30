747e05c7f9679d67870374b1aa9df215659b6b07
austinsanfran <40837514+austinsanfran@users.noreply.github.com>
2026-06-30 17:20:10 -0600

    Working Netmiko script - show interfaces from VyOS

--- netmiko_test.py
+++ netmiko_test.py
@@ -1,12 +1,17 @@
 from netmiko import ConnectHandler
+import time
+
 device = {
-    'device_type': 'cisco_ios',
-    'host': '127.0.0.1',
-    'username': 'admin',
-    'password': 'admin',
+    'device_type': 'vyos',
+    'host': '192.168.54.129',
+    'username': 'vyos',
+    'password': 'vyos',
+    'global_cmd_verify': False,
 }
 
 connection = ConnectHandler(**device)
-output = connection.send_command('show version')
+connection.write_channel('show interfaces\n')
+time.sleep(10)
+output = connection.read_channel()
 print(output)
 connection.disconnect()
